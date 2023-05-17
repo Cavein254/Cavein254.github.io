8f6ffe18749d894d8c96ba6492e6e4d87b09e56e
johnmuthua <caveomuthama@gmail.com>
2023-05-17 20:09:17 +0300

    chore completed initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary-100":"#1B1A17",
+        "primary-200":"#1F1E1B",
+        "secondary-100":"#A35709",
+        "secondary-200":"#FF8303",
+        "tertiary":"#F0E3CA"
+      },
+      fontFamily:{
+        inconsolata: ['Inconsolata', "sans-serif"],
+        impact:['Impact', "sans-serif"]
+      },
+      screens:{
+        xs:"480px",
+        sm:"768px",
+        md:"1060px",
+      }
+    },
   },
   plugins: [],
 }
