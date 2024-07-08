ea0ef118f5605b22e922c289aeabf57e1bd49675
johnmuthua <caveomuthama@gmail.com>
2024-07-08 12:09:06 +0300

    update build path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/",
-  resolve:{
-    alias:[{find: "@", replacement:path.resolve(__dirname, "src")}]
-  }
-})
+  base: "/",
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  build: {
+    outDir: "./build",
+    emptyOutDir: true,
+  },
+});
