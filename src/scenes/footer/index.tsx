ab32bac33cab4cb04ed559a27338f126a8c8f558
johnmuthua <caveomuthama@gmail.com>
2024-07-08 11:51:33 +0300

    Add new quote

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -4,7 +4,9 @@ const Footer = () => {
   return (
     <div className="my-6">
       <p className="text-center text-[1.2rem] font-bold text-white">
-        Do one thing every day that scares you. -Eleanor Roosevelt
+        "Your work is going to fill a large part of your life, and the only way
+        to be truly satisfied is to do what you believe is great work. And the
+        only way to do great work is to love what you do." - Steve Jobs
       </p>
       <p className="text-center text-[1.2rem] font-bold text-secondary-200">
         &copy; {year} - All rights reserved.
